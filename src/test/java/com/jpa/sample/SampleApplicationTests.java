427e7b7c2048db0c9a7b73654addab766c93d3db
ydj1215 <ydjdj2@naver.com>
2023-11-22 12:25:40 +0900

    20231122

--- src/test/java/com/jpa/sample/SampleApplicationTests.java
+++ src/test/java/com/jpa/sample/SampleApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SampleApplicationTests {
-
+	// 테스트 하는 방법 : 레포지토리 만들고, 클래스 이름에 마우스 오른쪽 클릭 후 go to test 클릭
 	@Test
 	void contextLoads() {
 	}
