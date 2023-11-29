582c56675130cdee5b9c4c8bf8ef005e90a22874
ydj1215 <ydjdj2@naver.com>
2023-11-29 10:28:41 +0900

    20231129

--- src/main/java/com/jpa/sample/entity/OrderItem.java
+++ src/main/java/com/jpa/sample/entity/OrderItem.java
@@ -19,6 +19,11 @@ public class OrderItem {
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY) // OrderRepositoryTest와 함께 보기
+    // fetch = FetchType.LAZY : 지연 로딩을 설정
+    // 즉 OrderItem 엔티티를 조회할 때, Item과 Order 엔티티는 같이 조회되지 않고,
+    // orderItem.getOrder()과 같은 코드가 실행될 때, order테이블이 로드된다.
+
+    // 만약 fetch = FetchType.EAGER로 설정한다면, OrderItem을 조회할 때, Item과 Order이 함께 조회되며, 이를 즉시 로딩이라고 한다.
     @JoinColumn(name = "item_id")
     private Item item;
 
--- src/test/java/com/jpa/sample/repository/OrderRepositoryTest.java
+++ src/test/java/com/jpa/sample/repository/OrderRepositoryTest.java
@@ -11,9 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityNotFoundException;
@@ -83,6 +83,7 @@ class OrderRepositoryTest {
 
     public Order createOrder() {
         Order order = new Order();
+        order.setOrderDate(LocalDateTime.now());
 
         for (int i = 0; i < 3; i++) {
             Item item = createItem();
@@ -117,16 +118,41 @@ class OrderRepositoryTest {
         em.flush();
     }
 
+    // 지연 로딩 : JPA에서 제공하는 기능으로, 연관된 엔티티를 실제로 사용하는 순간에 데이터베이스로부터 로드하는 방법
+    // 지연 로딩은 불필요한 데이터 베이스 접근을 줄여, 성능적인 향상을 시켜준다.
     @Test
     @DisplayName("지연 로딩 테스트") // LAZY 전략
     public void lazyLoadingTest() {
         Order order = this.createOrder();
         Long orderItemId = order.getOrderItemList().get(0).getId(); // 아이템 주문 리스트의 첫번째 아이템을 가져온다.
-        em.flush(); // commit
-        em.clear(); //
+        em.flush(); // 영속성 컨텍스트를 즉시 데이터 베이스에 적용
+        em.clear();  // 영속성 컨텍스트 비워주기
+
         OrderItem orderItem = orderItemRepository.findById(orderItemId).orElseThrow(EntityNotFoundException::new);
-        log.warn("로거 : " + orderItem.getOrder().getClass());
+
+        // 지연 로딩 작동 확인을 위한 로깅
+        log.warn("로거 : " + orderItem.getOrder().getClass()); // OrderItem 객체의 order 필드의 실제 클래스 확인
+        /*
+        지연 로딩이 적용되면, order 필드는 실제 Order 객체 대신 프록시 객체를 가지게 된다.
+        프록시 객체는 실제 엔티티 객체에 대한 참조를 유지하면서,
+        실제 엔티티의 데이터가 필요한 시점에 데이터베이스에서 데이터를 로드하는 역할을 한다.
+        따라서 이 로그 구문을 통해서 order 필드가 프록시 객체를 가지고 있는지 확인할 수 있다.
+        */
+
+        /*
+        지연 로딩을 사용할 경우, 처음에는 프록시 객체만 생성되고 그 프록시 객체가 실제 엔티티의 데이터를 갖고 있지는 않다.
+        이 프록시 객체는 실제 엔티티에 대한 참조만을 가지고 있다.
+        그리고 실제로 그 엔티티의 데이터에 접근해야 할 경우, 즉 아래의 코드가 실행될때와 같은 경우에,
+        그제서야 데이터 베이스에서 실제 데이터를 조회하여 프록시 객체에 로드하게 된다.
+        */
         log.warn("지연 로딩 시간 확인 : " + orderItem.getOrder().getOrderDate());
+
+        /*
+        예를 들자면 order가 member를 참조하고 있을 때,
+        order객체를 호출하면 member의 실제 데이터는 불러와지지 않지만,
+        프록시가 ("이따가 member데이터를 가져올수도 있겠다" = member객체에 대한 참조) 는 알고 있는 것이다.
+        고로 getOrderId() 와 같은 메소드를 호출했을때, 프록시가 없었더라면의 경우보다 더 빠르게 member의 실제 데이터를 불러올 수 있을 것이다. (성능 최적화)
+        */
     }
 }
 
